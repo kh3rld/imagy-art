9831ac2a13bf9d322677ccd80769cdcbcf97a1e3
kh3rld <kherld.hussein@gmail.com>
2024-12-03 18:05:54 +0300

    Feat(load): Implement a func to load image from a file

---
+++ utils/image_utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"image"
+	"image/png"
+	"os"
+)
+
+// LoadImage loads an image from a file
+func LoadImage(filename string) (image.Image, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	img, err := png.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
